ba4bebd7e5f85b24626d120a4a8b8382e269bfe8
Makedon3 <Makedon3@yandex.ru>
2023-04-27 15:53:58 +0300

    3.1.5 1 исправление

--- src/main/java/ru/kata/spring/boot_security/demo/util/Mapper.java
+++ src/main/java/ru/kata/spring/boot_security/demo/util/Mapper.java
@@ -1,9 +1,10 @@
 package ru.kata.spring.boot_security.demo.util;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.dto.UserDTO;
 import ru.kata.spring.boot_security.demo.model.User;
-
+@Component
 public class Mapper {
 
     private final ModelMapper modelMapper;
